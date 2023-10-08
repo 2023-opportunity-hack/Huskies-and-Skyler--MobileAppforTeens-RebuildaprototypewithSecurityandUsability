bc3851885af5e1556b613c1394862ca63992e6df
RachelFeng924 <rachel2298277@gmail.com>
2023-10-08 09:26:33 -0700

    fixed images in pubspec.yaml

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -41,120 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Safe Space"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Safe Space',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30.0,
-                fontFamily: "Tahoma",
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0), // Add outer padding
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/first');
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white, padding: EdgeInsets.symmetric(
-                  horizontal: 25.0, // Adjust horizontal padding as needed
-                  vertical: 10.0,   // Adjust vertical padding as needed
-                ),
-                  backgroundColor: Colors.red,
-                ),
-                child: Text(
-                  "First Screen",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontFamily: "Times New Roman",
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-
-          ],
-        ),
-      ),
-    ); // This trailing comma makes auto-formatting nicer for build methods.
-  }
-
-}
-
-
-//
-// class BaseLayout extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context){
-//     return Scaffold(
-//       body: Container(
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage("assets/safespacebg.png"),
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           children: [
-//             // Add other widgets here
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-import 'package:flutter/material.dart';
-import 'package:test_app/tictactoe.dart';
-import 'package:test_app/Firstpage.dart';
-import 'package:test_app/Secondpage.dart';
-import 'package:test_app/home_temp.dart';
-
-
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/first': (context) => Firstpage(),
-        '/second': (context) => Secondpage(),
-        '/tictactoe': (context) => Tictactoe(),
-        '/home_temp': (context) => Hometemp(),
-      },
-      title: 'main page for safespace',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
 
       appBar: AppBar(
         title: Text("Safe Space"),
@@ -201,9 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 style: TextButton.styleFrom(
                   foregroundColor: Colors.white, padding: EdgeInsets.symmetric(
-                    horizontal: 55.0, // Adjust horizontal padding as needed
-                    vertical: 20.0,   // Adjust vertical padding as needed
-                  ),
+                  horizontal: 55.0, // Adjust horizontal padding as needed
+                  vertical: 20.0,   // Adjust vertical padding as needed
+                ),
                   backgroundColor: Colors.deepPurple,
                 ),
                 child: Text(
